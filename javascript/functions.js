0f09798f964f0e2f98d942a3e1403775bbec8293
Martin Theimer <pappkamerad@decoded.net>
2004-06-16 19:39:45 +0000

    added headertext

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,3 +1,17 @@
+/**
+ * functions.js -- javascript functions
+ *
+ * Copyright (C) 2003, 2004 Martin Theimer
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * Contact: Martin Theimer <pappkamerad@decoded.net>
+ *
+ * The latest version of phpAutoGallery can be obtained from:
+ * http://sourceforge.net/projects/phpautogallery
+ *
+ * $Id$
+ */
+
 function Go(x) {
  if(x == "nothing") {
    document.forms[0].reset();
