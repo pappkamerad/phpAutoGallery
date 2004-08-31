3d7a898b5b39fd34dfa281709441e5dbd6744936
Martin Theimer <pappkamerad@decoded.net>
2004-08-31 20:56:17 +0000

    fixed jump-function for frames usage

--- javascript/functions.js
+++ javascript/functions.js
@@ -18,10 +18,8 @@ function Go(x) {
    document.forms[0].elements[0].blur();
    return;
  }
- else if(x == "end")
-   top.location.href = parent.frames[1].location;
  else {
-   top.location.href = x;
+   this.location.href = x;
    document.forms[0].reset();
    document.forms[0].elements[0].blur();
  }
